9b212e1efb94b1c26bda18c9b7038f0a265192b7
Alok Singh <AlokSingh1200@gmail.com>
2025-07-05 20:45:07 +0530

    learn useEffact

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,29 +1,36 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Counter = () => {
+  // mounting rerendaring unmounting
   const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count + 1);
-  }
+  useEffect(function () {
+    setInterval(function () {
+      setCount((count) => count + 1);
+    }, 100);
+  }, []);
 
-  function decreaseCount() {
-    setCount(count - 1);
-  }
+  //   function increaseCount() {
+  //     setCount(count + 1);
+  //   }
 
-  function zeroCount() {
-    setCount(0);
-  }
+  //   function decreaseCount() {
+  //     setCount(count - 1);
+  //   }
+
+  //   function zeroCount() {
+  //     setCount(0);
+  //   }
 
   return (
     <>
       <h1 id="text">{count}</h1>
-      <button onClick={increaseCount}>count increase</button>
+      {/* <button onClick={increaseCount}>count increase</button> */}
 
-      <button onClick={decreaseCount}>decrease increase</button>
+      {/* <button onClick={decreaseCount}>decrease increase</button> */}
 
-      <button onClick={zeroCount}>zero increase</button>
+      {/* <button onClick={zeroCount}>zero increase</button> */}
     </>
   );
 };
